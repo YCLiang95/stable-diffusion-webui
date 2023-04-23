c1c72a988090e13f3c40de5c227e4f80b8901066
Yecheng Liang <ycliang95@gmail.com>
2023-04-23 20:32:21 +0800

    local sub module 2

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=--xformers --no-half --precision full --no-half-vae --lowvram --theme dark
+set COMMANDLINE_ARGS=--xformers --no-half --precision full --no-half-vae --theme dark
 
 call webui.bat
